5faabd73ca99ce5f87b7cb48caa00c8ac8b94a65
Boon Hsuan Chua <puppylongears@gmail.com>
2024-12-10 18:49:13 +0800

    Create main page

---
+++ scheduler-app/src/Components/Header.tsx
@@ -0,0 +1,18 @@
+
+// Example of a React component (essentially a function where you return HTML)
+function Header() {
+    return (
+        <header>
+            <h1>Scheduler App</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
